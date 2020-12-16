6d1ac83294a0bfecf79021cd044959cbd8d513b7
hubvue <1191340528@qq.com>
2020-12-17 01:37:03 +0800

    chore: init

---
+++ src/2.两数相加.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=typescript
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+
+};
+// @lc code=end
+
---
+++ src/28.实现-str-str.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=typescript
+ *
+ * [28] 实现 strStr()
+ */
+
+// @lc code=start
+function strStr(haystack: string, needle: string): number {
+
+};
+// @lc code=end
+
